a982bbfb0714f35b3848e5382c34e8025813bdd5
Abhishek Sharma <sharma.abhishek208@gmail.com>
2021-12-28 17:41:55 +0530

    Added Welcome Screen

---
+++ lib/Screens/WelcomeScreen.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff79201B),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset("assets/Icons/logo.png"),
+          ],
+        ),
+      ),
+    );
+  }
+}
